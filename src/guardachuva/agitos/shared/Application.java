532f50eb9daf060d53d7e37bfe12d8d36d16c013
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-03-15 02:24:22 -0300

    Minor refactoring.

--- src/guardachuva/agitos/shared/Application.java
+++ src/guardachuva/agitos/shared/Application.java
@@ -4,31 +4,19 @@ import java.util.Date;
 
 public interface Application {
 
-	public abstract SessionToken createNewUser(String name, String userName,
-			String password, String email) throws ValidationException, UserAlreadyExistsException;
+	SessionToken createNewUser(String name, String userName,	String password, String email) throws ValidationException, UserAlreadyExistsException;
+	SessionToken authenticate(String email, String password) throws UnauthorizedBusinessException;
+	UserDTO getLoggedUserOn(SessionToken session) throws UnauthorizedBusinessException;
+	void logout(SessionToken session) throws UnauthorizedBusinessException;
 
-	public abstract SessionToken authenticate(String email, String password)
-			throws UnauthorizedBusinessException;
+	EventDTO[] getEventsForMe(SessionToken session) throws UnauthorizedBusinessException;
+	void createEvent(SessionToken session, String description, Date date) throws BusinessException;
+	void removeEventForMe(SessionToken session, int id) throws BusinessException;
 
-	public abstract EventDTO[] getEventsForMe(SessionToken session) throws UnauthorizedBusinessException;
+	void addContactsToMe(SessionToken session, String contact_mail, String linkToApplication) throws ValidationException, Exception;
+	UserDTO[] getContactsForMe(SessionToken session) throws UnauthorizedBusinessException;
+	void deleteContactForMe(SessionToken session, String email) throws BusinessException;
 
-	public abstract void createEvent(SessionToken session, String description, Date date)
-			throws BusinessException;
-
-	public abstract void removeEventForMe(SessionToken session, int id)
-			throws BusinessException;
-
-	public abstract void addContactsToMe(SessionToken session, String contact_mail,
-			String linkToApplication) throws ValidationException, Exception;
-
-	public abstract void deleteContactForMe(SessionToken session, String email) throws BusinessException;
-
-	public abstract UserDTO[] getContactsForMe(SessionToken session) throws UnauthorizedBusinessException;
-
-	public abstract void ignoreProducerForMe(SessionToken session, String email) throws ValidationException, BusinessException;
-
-	public void logout(SessionToken session) throws UnauthorizedBusinessException;
-
-	public abstract UserDTO getLoggedUserOn(SessionToken session) throws UnauthorizedBusinessException;
+	void ignoreProducerForMe(SessionToken session, String email) throws ValidationException, BusinessException;
 
 }
--- src/guardachuva/agitos/shared/SessionToken.java
+++ src/guardachuva/agitos/shared/SessionToken.java
@@ -5,41 +5,24 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class SessionToken implements IsSerializable {
 
-	private static final long serialVersionUID = 1L;
-
-	private String _token;
-	
-	public SessionToken() {
-		
-	}
+	private final String _token;
 
 	public SessionToken(String token) {
+		if (token == null) throw new IllegalArgumentException();
 		_token = token;
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((_token == null) ? 0 : _token.hashCode());
-		return result;
+		return _token.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SessionToken other = (SessionToken) obj;
-		if (_token == null) {
-			if (other._token != null)
-				return false;
-		} else if (!_token.equals(other._token))
-			return false;
-		return true;
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		return _token != null && _token.equals(((SessionToken)obj)._token);
 	}
 
 	@Override
@@ -50,5 +33,8 @@ public class SessionToken implements IsSerializable {
 	public String getToken() {
 		return _token;
 	}
+
 	
+	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unused") private SessionToken() { _token = null; }
 }
