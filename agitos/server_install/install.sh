47d582c4ccaff22b4b5ee73ad31a2bda0eb4f58c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-04-01 03:37:07 -0300

    server_install moved from agitosOld to agitos

--- agitos/server_install/install.sh
+++ agitos/server_install/install.sh
@@ -20,18 +20,21 @@ rm /root/.ssh/known_hosts
 cp id_rsa      /root/.ssh/id_rsa
 cp id_rsa.pub  /root/.ssh/id_rsa.pub
 cp known_hosts /root/.ssh/known_hosts
+chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/known_hosts
 echo .
 echo ----------------------
-echo retriving build.xml
+echo retreiving build.xml
 echo ----------------------
 mkdir /agitos_server
-mkdir /agitos_server/git_repos
-cd /agitos_server/git_repos
+mkdir /agitos_server/git_repositories
+cd /agitos_server/git_repositories
 git clone git@github.com:teamware/agitos.git
 git clone git@github.com:bihaiko/sneer.git
 echo .
 echo ----------------------
-echo hudson
+echo Tomcat
 echo ----------------------
 cd /agitos_server
 wget http://linorg.usp.br/apache/tomcat/tomcat-7/v7.0.11/bin/apache-tomcat-7.0.11.tar.gz
@@ -39,5 +42,4 @@ tar -xf *.tar.gz
 rm *.tar.gz
 mv *tomcat* tomcat
 echo .
-wget http://java.net/projects/hudson/downloads/download/war/hudson-1.398.war
-mv *hudson*.war /agitos_server/tomcat/webapps/hudson.war
+
