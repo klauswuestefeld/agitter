92e6083b6aec411e21614b674136751a4fcc63f9
Leonardo Nunes <leonardonunes@gmail.com>
2011-04-19 11:47:52 -0300

    Spike - Started to put the domain objects in the current UI.

--- eclipse-project/src/agitter/ui/EventPanel.java
+++ eclipse-project/src/agitter/ui/EventPanel.java
@@ -1,5 +1,9 @@
 package agitter.ui;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
@@ -9,13 +13,15 @@ import com.vaadin.ui.Panel;
 
 public class EventPanel extends CustomComponent {
 
+	private DateFormat _dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
 	private Panel mainPanel;
 
 	public EventPanel(agitter.Event event) {
 		mainPanel = new Panel();
 		mainPanel.addComponent(new Label(event.description()));
-		mainPanel.addComponent(new Label("klaus@gmail.com"));
-		mainPanel.addComponent(new Label("Format: " + event.datetime())); //TODO - Date format applies here
+		mainPanel.addComponent(new Label("everybody@aggiter.com"));
+		mainPanel.addComponent(new Label(this._dateFormat.format(new Date(event.datetime()))));
 		mainPanel.addComponent(removeButton());
 		setCompositionRoot(mainPanel);
 	}
@@ -24,7 +30,7 @@ public class EventPanel extends CustomComponent {
 	private Button removeButton() {
 		Button result = new Button("X");
 		result.addListener(new ClickListener() {  private static final long serialVersionUID = 1L;  @Override public void buttonClick(ClickEvent event) {
-			System.out.println("Event removed");
+			System.out.println("Event to be removed or hid?");
 		}});
 		return result;
 	}
--- eclipse-project/src/agitter/ui/InvitePanel.java
+++ eclipse-project/src/agitter/ui/InvitePanel.java
@@ -1,56 +1,79 @@
 package agitter.ui;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.util.Date;
 
 import agitter.EventHome;
 import agitter.EventHomeImpl;
 import agitter.util.SystemClock;
-import com.vaadin.ui.Button;
+import com.vaadin.terminal.UserError;
+import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.PopupDateField;
-import com.vaadin.ui.TextArea;
 
 public class InvitePanel extends CustomComponent {
 
-	private TextArea description = new TextArea(null, "Descricao do seu agito...");
-	private PopupDateField _date = new PopupDateField();
-	private DateFormat _dateFormat = DateFormat.getDateTimeInstance();
-	private EventHome _eventHome = new EventHomeImpl(new SystemClock()); //TODO - Spike to use the home in the current UI.
+	private static final long serialVersionUID = 1L;
+	private static final String INVALID_DATE_MESSAGE = "Data inválida";
+	private static final String INVALID_DESCRIPTION_MESSAGE = "Descreva seu agito!";
+	private static final int WARNING_MESSAGES_DELAY = 2500;
+	private static final String WARNING_MESSAGE_TITLE = "Atenção";
+
+	private TextArea _descriptionTextArea = new TextArea();
+	private PopupDateField _dateField = new PopupDateField();
+	private EventHome _eventHome = new EventHomeImpl(new SystemClock()); //TODO - This is just a spike to use the home in the current UI.
 
 	public InvitePanel(final EventAdder adder) {
 		Panel panel = new Panel();
-		panel.addComponent(description);
-		panel.addComponent(_date);
+		panel.addComponent(_descriptionTextArea);
+		panel.addComponent(_dateField);
 		panel.addComponent(inviteButton(adder));
 		setCompositionRoot(panel);
 
-		_date.setResolution(PopupDateField.RESOLUTION_MIN);
+		_dateField.setResolution(PopupDateField.RESOLUTION_MIN);
+		_descriptionTextArea.setNullRepresentation("Descrição do seu agito ...");
+		_descriptionTextArea.setValue(null);
 
-		//TODO - Im my workstation the format is the following, we have to see how to use locales correctly in vaadin.
-		//The setLocale and setFormat in the PopupDateField only sets the view for the client side, the toString (server side) method returns a date in the format bellow.
-		//_dateFormat =  ...  //"Tue Apr 19 10:23:17 BRT 2011"
-		
 	}
 
 	private Button inviteButton(final EventAdder adder) {
 		Button result = new Button("Invite!");
-		result.addListener(new ClickListener() { private static final long serialVersionUID = 1L;  @Override public void buttonClick(ClickEvent event) {
+		result.addListener(new ClickListener() { private static final long serialVersionUID = 1L; @Override public void buttonClick(ClickEvent event) {
 			try {
-				adder.add(InvitePanel.this._eventHome.create(description.toString(), _dateFormat.parse(_date.toString()).getTime()));
-			} catch(ParseException e) {
-				System.out.println("Date format shout be in the format of the following string: "+ _date.toString());
-				e.printStackTrace();
+				final String description = getEventDescription();
+				final Date eventDate = getEventDate();
+				adder.add(InvitePanel.this._eventHome.create(description, eventDate.getTime()));
+			} catch (Exception exc) {
+				InvitePanel.this.showErrorMessage(exc.getMessage());
 			}
 		}});
 		return result;
 	}
-	
 
-	private static final long serialVersionUID = 1L;
+	private String getEventDescription() {
+		final String description = (String) _descriptionTextArea.getValue();
+		if(description==null) {
+			_descriptionTextArea.setComponentError(new UserError(INVALID_DESCRIPTION_MESSAGE));
+			throw new RuntimeException(INVALID_DESCRIPTION_MESSAGE); //TODO - Define some error msgs mechanism.
+		}
+		_descriptionTextArea.setComponentError(null);
+		return description;
+	}
+
+	private Date getEventDate() {
+		final Date eventDate = (Date) _dateField.getValue();
+		if(eventDate==null) {
+			_dateField.setComponentError(new UserError(INVALID_DATE_MESSAGE));
+			throw new RuntimeException(INVALID_DATE_MESSAGE); //TODO - Define some error msgs mechanism.
+		}
+		_dateField.setComponentError(null);
+		return eventDate;
+	}
+
+	private void showErrorMessage(String message) {
+		Window.Notification notif = new Window.Notification(WARNING_MESSAGE_TITLE, message, Window.Notification.TYPE_WARNING_MESSAGE);
+		notif.setPosition(Window.Notification.POSITION_CENTERED);
+		notif.setDelayMsec(WARNING_MESSAGES_DELAY);
+		this.getWindow().showNotification(notif);
+	}
+
 }
