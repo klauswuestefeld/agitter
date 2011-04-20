f2f804950a94f49a2dd8bd8822935edfb57b2846
Leonardo Nunes <leonardonunes@gmail.com>
2011-04-20 19:48:25 -0300

    AgitterApplication -> AgitterVaadinApplication

---
+++ eclipse-project/src/agitter/ui/AgitterVaadinApplication.java
@@ -0,0 +1,29 @@
+package agitter.ui;
+
+import java.io.File;
+
+import agitter.util.PrevaylerBootstrap;
+
+import com.vaadin.Application;
+
+public class AgitterVaadinApplication extends Application {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() {
+		initSystem();
+		AgitterPresenterImpl presenter = new AgitterPresenterImpl(PrevaylerBootstrap.execution());
+		AgitterMainWindow window = new AgitterMainWindow(presenter);
+		setMainWindow(window);
+	}
+	private void initSystem() {
+		try {
+			final File tmpFolder = new File("/agitterrepo"); //TODO
+			if(!tmpFolder.exists()) { tmpFolder.mkdir(); }
+			PrevaylerBootstrap.open(tmpFolder);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
