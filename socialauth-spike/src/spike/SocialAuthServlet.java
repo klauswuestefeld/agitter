075170256e11eec575b98ddf676064ceb1c40f03
unknown <ppeccin@.(none)>
2011-04-26 17:56:48 -0300

    Criando socialauth spike

---
+++ socialauth-spike/src/spike/SocialAuthServlet.java
@@ -0,0 +1,17 @@
+package spike;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SocialAuthServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getOutputStream().print("PECCIN");
+	}
+	
+}
---
+++ socialauth-spike/src/spike/main/SpikeMain.java
@@ -0,0 +1,33 @@
+package spike.main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class SpikeMain {
+
+	private static String contextPath = "/";
+	private static String resourceBase = "spike-web";
+	private static int httpPort = 8888;
+
+	public static void main(String[] args) throws Exception {
+		runJetty();
+	}
+
+	private static void runJetty() throws Exception, InterruptedException {
+		Server server = new Server(httpPort);
+		server.setHandler(webapp());
+		server.start();
+		
+		System.out.println("Started Jetty " + Server.getVersion() + ", go to http://localhost:" + httpPort + contextPath);
+		server.join();
+	}
+
+	private static WebAppContext webapp() {
+		WebAppContext result = new WebAppContext();
+		result.setContextPath(contextPath);
+		result.setResourceBase(resourceBase);
+		result.setClassLoader(Thread.currentThread().getContextClassLoader());
+		return result;
+	}
+	
+}
