3cbd04e13986043ae0e0133ec758f89fdc17f899
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-05-19 18:26:57 -0300

    Minor warning cleanup

--- eclipse-project/src/agitter/tests/PeriodicScheduleEmailTest.java
+++ eclipse-project/src/agitter/tests/PeriodicScheduleEmailTest.java
@@ -2,32 +2,31 @@ package agitter.tests;
 
 import java.io.IOException;
 
-import agitter.PeriodicScheduleNotificationDaemon;
-import agitter.domain.Agitter;
-import agitter.domain.AgitterImpl;
-import agitter.domain.events.Event;
-import agitter.domain.users.User;
-import agitter.email.tests.EmailSenderMock;
 import org.junit.Test;
+
 import sneer.foundation.lang.Clock;
 import sneer.foundation.lang.exceptions.Refusal;
 import sneer.foundation.testsupport.CleanTestBase;
+import agitter.PeriodicScheduleNotificationDaemon;
+import agitter.domain.Agitter;
+import agitter.domain.AgitterImpl;
+import agitter.domain.users.User;
+import agitter.email.tests.EmailSenderMock;
 
-@SuppressWarnings({"MagicNumber"})
 public class PeriodicScheduleEmailTest extends CleanTestBase {
 
 	@Test
 	public void periodicSchedule() throws Refusal, IOException {
 		Clock.setForCurrentThread(9);
 		Agitter agitter = new AgitterImpl();
-		User guest = agitter.users().signup("leo", "leo@email.com", "pass");
+		agitter.users().signup("leo", "leo@email.com", "pass");
 		User host = agitter.users().signup("klaus", "klaus@email.com", "pass");
 
 
-		Event e1 = agitter.events().create(host, "e1", 10L);
-		Event e2 = agitter.events().create(host, "e2", 11L);
-		Event e3 = agitter.events().create(host, "e3", 12L);
-		Event e4 = agitter.events().create(host, "e4", 13L);
+		agitter.events().create(host, "event1", 10L);
+		agitter.events().create(host, "event2", 11L);
+		agitter.events().create(host, "event3", 12L);
+		agitter.events().create(host, "event4", 13L);
 
 		Clock.setForCurrentThread(11);
 
@@ -37,7 +36,7 @@ public class PeriodicScheduleEmailTest extends CleanTestBase {
 
 		assertEquals("leo@email.com", emailSenderMock.to());
 		assertEquals("Agitos da Semana", emailSenderMock.subject());
-		final String body = "Olá leo, seus amigos estão agitando e querem você lá: <br/><BR><BR>klaus - e2<BR><BR>klaus - e3<BR><BR>klaus - e4<BR><a href=\"http://agitter.com\">Acesse o Agitter</a> para ficar por dentro e também convidar seus amigos para festas, eventos, saídas ou qualquer outro tipo de agito.<BR><BR><BR>Saia da Internet. Agite!   \\o/<BR>Equipe Agitter<BR><a href=\"http://agitter.com\">agitter.com</a><BR/>";
+		final String body = "Olá leo, seus amigos estão agitando e querem você lá: <br/><BR><BR>klaus - event2<BR><BR>klaus - event3<BR><BR>klaus - event4<BR><a href=\"http://agitter.com\">Acesse o Agitter</a> para ficar por dentro e também convidar seus amigos para festas, eventos, saídas ou qualquer outro tipo de agito.<BR><BR><BR>Saia da Internet. Agite!   \\o/<BR>Equipe Agitter<BR><a href=\"http://agitter.com\">agitter.com</a><BR/>";
 		assertEquals(body, emailSenderMock.body());
 
 	}
