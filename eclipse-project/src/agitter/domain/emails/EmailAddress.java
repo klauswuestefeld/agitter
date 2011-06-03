5e353ed4f26462e619734381d962383c26c6b4ed
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2011-06-03 19:51:21 -0300

    Emails are saved lowercase

--- eclipse-project/src/agitter/domain/emails/EmailAddress.java
+++ eclipse-project/src/agitter/domain/emails/EmailAddress.java
@@ -5,20 +5,26 @@ import java.io.Serializable;
 import sneer.foundation.lang.Immutable;
 import sneer.foundation.lang.exceptions.Refusal;
 
-public class EmailAddress extends Immutable implements Serializable {
+public class EmailAddress extends Immutable implements Comparable<EmailAddress>, Serializable {
 
 	private final String emailAddress;
 
 	
 	public EmailAddress(String emailAddress) throws Refusal {
 		AddressValidator.validateEmail(emailAddress);
-		this.emailAddress = emailAddress;
+		this.emailAddress = emailAddress.toLowerCase();
 	}
 
 	
 	@Override
 	public String toString() {
 		return emailAddress;
+	}
+
+
+	@Override
+	public int compareTo(EmailAddress other) {
+		return emailAddress.compareTo( other.emailAddress );
 	}
 
 }
