875dcb3f0008ef187bc5bf62a5fdd43f56532e32
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2011-06-03 20:01:38 -0300

    Minor cleanup

--- bubble/src/sneer/foundation/testsupport/CleanTestBase.java
+++ bubble/src/sneer/foundation/testsupport/CleanTestBase.java
@@ -7,7 +7,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import sneer.foundation.lang.Clock;
 
