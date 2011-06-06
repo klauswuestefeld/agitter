7c39762d60ef8d9241fc62bc7102397c312a98cd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-06-06 03:49:17 -0300

    New ProcessReplacer unit tested.

--- bubble/src/infra/processreplacer/ProcessReplacer.java
+++ bubble/src/infra/processreplacer/ProcessReplacer.java
@@ -1,17 +1,19 @@
 package infra.processreplacer;
 
+import infra.logging.LogInfra;
+
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
 
-import sneer.foundation.lang.Closure;
-import sneer.foundation.lang.ClosureX;
-
-/** Kills previous instances of this process. It connects to the ProcessReplacerPort (system property). It then listens on that port to kill itself (System.exit(0)) if it receives a connection.*/
 public class ProcessReplacer {
 
 	private static final int _port = Integer.parseInt(property("Port", "44111"));
+
+	private static final int READY_TO_RETIRE = 77;
+	private static final int PLEASE_RETIRE = 88;
 
 	
 	public interface ReplaceableProcess {
@@ -23,190 +25,165 @@ public class ProcessReplacer {
 	}
 
 
+	/** Retires previous ReplaceableProcess instances. It connects to the ProcessReplacerPort (system property). It then listens on that port to retire process if it receives a connection.*/
 	public ProcessReplacer(ReplaceableProcess process) {
 		this.process = process;
 		
 		try {
 			tryToTakeOver();
 		} catch (Exception e) {
+			log(e, "Process replacer unable to take over. Retiring process...");
 			this.process.retire();
 		}
 	}
 
 
-	private void tryToTakeOver() {
-		preparePreviousProcessToRetireIfNecessary();
-		this.process.prepareToTakeOver();
-		retirePreviousProcessIfNecessary();
-		this.process.takeOver();
+	private void log(Exception e, String message) {
+		LogInfra.getLogger(this).log(Level.SEVERE, message, e);
 	}
 
-	
+
 	private final ReplaceableProcess process;
 
+	private Socket previousProcess;
+	private ServerSocket mutex;
 	
-	private void preparePreviousProcessToRetireIfNecessary() {
+	
+	private void tryToTakeOver() throws IOException {
+		preparePreviousProcessToRetireIfNecessary();
+		process.prepareToTakeOver();
+		retirePreviousProcessIfNecessary();
+		startAcceptingRetirementRequests();
+		process.takeOver();
 	}
 
 	
-	private void retirePreviousProcessIfNecessary() {
-	}
-
-
-	
-	
-	
-	
-	
-	
-	
-	private static final int READY_TO_RETIRE = 77;
-	private static final int PLEASE_RETIRE = 88;
-
-
-
-	private static ServerSocket _serverSocket;
-	private static int _attemptCount = 0;
-	private static boolean _previousSessionKillAttempted;
-
-	private static ClosureX<Exception> preparationToTakeOver;
-	private static ClosureX<Exception> preparationToDie;
-	private static Closure recovery;
-
-	
-	public static void start() throws IOException {
-		killPreviousSessionIfNecessary();
-		startAcceptingKillRequests();
-	}
-
-
-	private static void startAcceptingKillRequests() throws IOException {
-		Thread t = new Thread() { @Override public void run() {
-			while (true)
-				acceptKillRequest();
-		}};
-		t.setDaemon(true);
-		t.start();
-	}
-
-
-	private static void killPreviousSessionIfNecessary() throws IOException {
-		while (true) {
-			try {
-				_serverSocket = new ServerSocket(_port);
-				break;
-			} catch (BindException be) {
-				sendKillRequestIfNecessary();
-				sleepOneSecond();
-				if (_attemptCount++ == 10)
-					fail(be.getMessage());
-			}
+	private void preparePreviousProcessToRetireIfNecessary() throws IOException {
+		try {
+			tryToAquireMutex();
+		} catch (BindException be) {
+			previousProcess = new Socket("127.0.0.1", _port);
+			waitForMessage(previousProcess, READY_TO_RETIRE);
 		}
+	}
+
+
+	private void retirePreviousProcessIfNecessary() throws IOException {
+		if (mutex != null) return;
+
+		sendMessage(previousProcess, PLEASE_RETIRE);
+		int attemptCount = 0;
+		while (mutex == null)
+			try {
+				tryToAquireMutex();
+			} catch (BindException be) {
+				waitOneSecond();
+				if (attemptCount++ > 20)
+					fail("Unable to aquire server socket after asking previous process to retire (tried " + attemptCount + " times).");
+			}
+	}
+
+	
+	private void tryToAquireMutex() throws IOException {
+		mutex = new ServerSocket(_port);
+	}
+	
+	
+	private void waitOneSecond() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) { }
+	}
+
+
+	private void startAcceptingRetirementRequests() throws IOException {
+		new Thread() { { setDaemon(true); } @Override public void run() {
+			while (!mutex.isClosed())
+				acceptRetirementRequest();
+		}}.start();
+	}
+	
+	
+	private void acceptRetirementRequest() {
+		Socket request = null;
+		try {
+			request = mutex.accept();
+			tryToHandleRetirementRequest(request);
+		} catch (Exception e) {
+			log(e, "Exception handling process retirement request.");
+		} finally {
+			if (request != null)
+				try { request.close(); } catch (IOException e) { }
+		}
+	}
+	
+	
+	private void tryToHandleRetirementRequest(Socket request) throws IOException {
+		checkFromSameMachine(request);
+		try {
+			tryToRetire(request);
+		} catch (Exception e) {
+			process.cancelRetirement();
+		}
+	}
+	
+	
+	private void tryToRetire(Socket request) throws IOException {
+		try {
+			process.prepareToRetire();
+		} catch (Exception e) {
+			throw new IOException("Preparation to retire failed. ", e);
+		}
+		sendMessage(request, READY_TO_RETIRE);
+		waitForMessage(request, PLEASE_RETIRE);
+		retire();
+	}
+	
+	
+	private void retire() {
+		close();
+		LogInfra.getLogger(this).log(Level.INFO, "Retiring...");
+		process.retire();
+	}
+
+
+	public void close() {
+		try { mutex.close(); } catch (IOException e) {}
+	}
+	
+	
+	private static void checkFromSameMachine(Socket request) throws IOException {
+		if (!"127.0.0.1".equals(request.getLocalAddress().getHostAddress()))
+			throw new IOException("Retirement request received from external machine.");
+	}
+	
+	
+	private static void waitForMessage(Socket socket, int expected) throws IOException {
+		int reply = socket.getInputStream().read();
+		if (reply != expected)
+			fail("Reply expected:" + expected + " Actual:" + reply);
+	}
+
+
+	private static void sendMessage(Socket socket, int message) throws IOException {
+		socket.getOutputStream().write(message);
+		socket.getOutputStream().flush();
 	}
 
 
 	private static void fail(String reason) throws IOException {
-		throw new IOException(className() + " unable to kill previous process. " + reason);
+		throw new IOException(className() + " unable to replace previous process. " + reason);
 	}
-
-
+	
+	
 	private static String property(String property, String defaultValue) {
 		String key = className() + property;
 		return System.getProperty(key, defaultValue);
 	}
-
-
+	
+	
 	private static String className() {
 		return ProcessReplacer.class.getSimpleName();
-	}
-
-
-	private static void sleepOneSecond() {
-		try {
-			Thread.sleep( 1000 * 1 );
-		} catch (InterruptedException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	
-	private static void acceptKillRequest() {
-		try {
-			tryToAcceptRequest();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-	
-	private static void tryToAcceptRequest() throws IOException {
-		Socket request = _serverSocket.accept();
-		if (isFromSameMachine(request))
-			try {
-				tryToDie(request);
-			} catch (Exception e) {
-				recovery.run();
-			}
-		request.close();
-	}
-
-
-	private static boolean isFromSameMachine(Socket request) {
-		return "127.0.0.1".equals(request.getLocalAddress().getHostAddress());
-	}
-
-
-	private static void tryToDie(Socket request) throws IOException {
-		try {
-			preparationToDie.run();
-		} catch (Exception e) {
-			throw new IOException("Preparation to die failed. ", e);
-		}
-		send(request, READY_TO_RETIRE);
-		waitForMessage(request, PLEASE_RETIRE);
-		die();
-	}
-
-
-	private static void send(Socket request, int message) throws IOException {
-		request.getOutputStream().write(message);
-		request.getOutputStream().flush();
-	}
-
-
-	private static void die() {
-		try { _serverSocket.close(); } catch (IOException e) {}
-		System.out.println(className() + " exiting.");
-		System.exit(0);
-	}
-
-	
-	private static void sendKillRequestIfNecessary() throws IOException {
-		if (_previousSessionKillAttempted) return;
-		_previousSessionKillAttempted = true;
-		sendKillRequest();
-	}
-
-
-	private static void sendKillRequest() throws IOException {
-		Socket request = new Socket("127.0.0.1", _port);
-		waitForMessage(request, READY_TO_RETIRE);
-
-		try {
-			preparationToTakeOver.run();
-		} catch (Exception e) {
-			fail("Preparation to take over failed. " + e.getMessage());
-		}
-		
-		send(request, PLEASE_RETIRE);
-		request.close();
-	}
-
-
-	private static void waitForMessage(Socket request, int expected) throws IOException {
-		int reply = request.getInputStream().read();
-		if (reply != expected)
-			fail("Reply expected:" + expected + " Actual:" + reply);
 	}
 
 }
--- bubble/src/infra/processreplacer/tests/ProcessReplacerTest.java
+++ bubble/src/infra/processreplacer/tests/ProcessReplacerTest.java
@@ -3,7 +3,6 @@ package infra.processreplacer.tests;
 import infra.processreplacer.ProcessReplacer;
 import infra.processreplacer.ProcessReplacer.ReplaceableProcess;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.foundation.testsupport.TestWithMocks;
@@ -13,7 +12,6 @@ public class ProcessReplacerTest extends TestWithMocks {
 	private final ReplaceableProcess process1 = mock("p1", ReplaceableProcess.class);
 	private final ReplaceableProcess process2 = mock("p2", ReplaceableProcess.class);
 
-	@Ignore
 	@Test
 	public void replacement() {
 		checking(new Expectations(){{
@@ -28,7 +26,8 @@ public class ProcessReplacerTest extends TestWithMocks {
 			exactly(1).of(process1).retire();inSequence();
 			exactly(1).of(process2).takeOver();inSequence();
 		}});
-		new ProcessReplacer(process2);
+		ProcessReplacer pr = new ProcessReplacer(process2);
+		pr.close();
 	}
 	
 }
