d4899084b2c96270b05fdd07be930f4e01855e58
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-06-07 02:54:40 -0300

    Latch opening support for mocking assynchronous calls.

--- bubble/src/sneer/foundation/testsupport/TestWithMocks.java
+++ bubble/src/sneer/foundation/testsupport/TestWithMocks.java
@@ -2,8 +2,12 @@ package sneer.foundation.testsupport;
 
 import org.jmock.Mockery;
 import org.jmock.Sequence;
+import org.jmock.api.Invocation;
 import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.internal.ReturnDefaultValueAction;
 import org.junit.After;
+
+import sneer.foundation.util.concurrent.Latch;
 
 
 public abstract class TestWithMocks extends CleanTestBase {
@@ -14,6 +18,13 @@ public abstract class TestWithMocks extends CleanTestBase {
 		/** Used to indicate methods have to be called in the correct sequence. */
 		protected void inSequence() {
 			inSequence(defaultSequence);
+		}
+
+		protected void open(final Latch latch) {
+			will(new ReturnDefaultValueAction() { @Override public Object invoke(Invocation invocation) throws Throwable {
+				latch.open();
+				return super.invoke(invocation);
+			}});
 		}
 	}
 
