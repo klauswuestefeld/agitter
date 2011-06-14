1b87ad4db5ed1cb2f7087a8b57687db2dadb85ce
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-06-14 10:04:05 -0300

    Minor

--- eclipse-project/src/agitter/domain/users/User.java
+++ eclipse-project/src/agitter/domain/users/User.java
@@ -3,17 +3,13 @@ package agitter.domain.users;
 public interface User {
 
 	public String username();
-
 	public String email();
-
-	public boolean isPassword(String password);
-
-	public String password();
-
 	public String fullName();
 
-	public boolean isInterestedInPublicEvents();
+	public String password();
+	public boolean isPassword(String password);
 
+	public boolean isInterestedInPublicEvents();
 	public void setInterestedInPublicEvents(boolean interested);
 
 }
