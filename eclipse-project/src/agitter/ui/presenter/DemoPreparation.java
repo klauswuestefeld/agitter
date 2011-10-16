1f0f36012a38e69108c7a05b433e135d24589924
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-10-16 18:21:49 -0300

    Richer demo

--- eclipse-project/src/agitter/ui/presenter/DemoPreparation.java
+++ eclipse-project/src/agitter/ui/presenter/DemoPreparation.java
@@ -1,6 +1,9 @@
 package agitter.ui.presenter;
 
 import static java.util.Collections.EMPTY_LIST;
+
+import java.util.Arrays;
+
 import sneer.foundation.lang.exceptions.Refusal;
 import agitter.domain.Agitter;
 import agitter.domain.emails.EmailAddress;
@@ -21,15 +24,15 @@ public class DemoPreparation {
 		User existing = domain.users().searchByEmail(DEMO_USER_EMAIL);
 		if (existing != null) return existing;
 
-		user = signupDemoUser();
+		user = signup(DEMO_USER_EMAIL);
 		populate();
 		return user;
 	}
 
 
-	private User signupDemoUser() {
+	private User signup(EmailAddress email) {
 		try {
-			return domain.users().signup(DEMO_USER_EMAIL, "demoPassword");
+			return domain.users().signup(email, "password123");
 		} catch (Refusal e) {
 			throw new IllegalStateException(e);
 		}
@@ -37,8 +40,14 @@ public class DemoPreparation {
 
 
 	private void populate() {
+		User anne = signup(EmailAddress.certain("anne@demo.com"));
+		createEvent(user, "Party!", 1000*60*60*24*10, anne);
+		createEvent(anne, "Movie", 1000*60*60*24*11, user);
+	}
+
+	private void createEvent(User owner, String description, int timeFromNow, User... invitees) {
 		try {
-			domain.events().create(user, "Party!", System.currentTimeMillis() + (1000*60*60*24*10), EMPTY_LIST, EMPTY_LIST);
+			domain.events().create(owner, description, System.currentTimeMillis() + timeFromNow, EMPTY_LIST, Arrays.asList(invitees));
 		} catch (Refusal e) {
 			throw new IllegalStateException(e);
 		}
