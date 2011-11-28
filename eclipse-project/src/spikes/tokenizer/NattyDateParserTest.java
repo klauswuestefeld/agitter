609d09b70dee56bf5ccce8869320c2d7b7d0dda1
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-11-28 19:33:08 -0200

    Minor

--- eclipse-project/src/spikes/tokenizer/NattyDateParserTest.java
+++ eclipse-project/src/spikes/tokenizer/NattyDateParserTest.java
@@ -12,21 +12,26 @@ import org.junit.Test;
 
 import sneer.foundation.lang.exceptions.Refusal;
 
+
 public class NattyDateParserTest {
+
 	private NattyDateParser p = new NattyDateParser();
 	private DateFormat formatter = new SimpleDateFormat();
+	
 	
 	public void assertFormattedDateEquals(Date d1, Date d2) {
 		Assert.assertEquals(formatter.format(d1), formatter.format(d2));
 	}
 
+	
 	public void assertFormattedDateEquals(GregorianCalendar d1, Date d2) {
 		Assert.assertEquals(formatter.format(d1.getTime()), formatter.format(d2));
 	}
 	
+	
 	@Test
 	public void test() throws Refusal {
-		if ("".isEmpty()) return; //Delete this line to run test.
+		if ("".isEmpty()) return; //Delete this line to run test. It was disabled because it is slow.
 			
 		GregorianCalendar knownDate = new GregorianCalendar();
 		knownDate.set(GregorianCalendar.WEEK_OF_YEAR, knownDate.get(GregorianCalendar.WEEK_OF_YEAR)+1);
@@ -39,8 +44,6 @@ public class NattyDateParserTest {
 		knownDate = new GregorianCalendar(2012, Calendar.JULY, 25, 12,00);
 		parsedDate = p.parsePost("Festa no dia 07/25/2012 12:00");
 		assertFormattedDateEquals(knownDate, parsedDate);
-		
-		
 	}
 
 }
