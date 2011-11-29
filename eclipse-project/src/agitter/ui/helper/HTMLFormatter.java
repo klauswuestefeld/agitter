9b65380bbbc1d367748f9467fa1ab23b6125907a
Vitor Pamplona <vitor@vitorpamplona.com>
2011-11-28 23:00:52 -0200

    Formatting links to HTML: Working: - www.teste.com.br - vitor@vitorpamplona.com - teste.com - http://blablabla.com - ftp://bblalsfasdf.com
    etc.

---
+++ eclipse-project/src/agitter/ui/helper/HTMLFormatter.java
@@ -0,0 +1,70 @@
+package agitter.ui.helper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HTMLFormatter {
+	
+	private final static String EMAIL_REGEX = "\\b(([a-zA-Z0-9_\\.\\-+])+@(([a-zA-Z0-9-])+\\.)+([a-zA-Z0-9]{2,4})+)";
+	private final static String WEB_REGEX = 
+			"\\b(" +
+			 "(https?|ftp|gopher|telnet|file|notes|ms-help)://" +
+			  "[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*" +
+			  "[-A-Za-z0-9+&@#/%=~_()|])";
+	
+	private final static String WWW_REGEX = 
+			"\\b(" +
+			  "(([-A-Za-z0-9+&@#/%?=~_()|!:,;]*)\\.)+" +
+			  "[-A-Za-z0-9+&@#/%?=~_()|!:,;]*" +
+			  "[-A-Za-z0-9+&@#/%=~_()|])";
+	
+	private final static String NO_HTML = "\\<.*?\\>";
+	
+	private final static String HTML_A_TAG = "\\<a.*?\\>.*\\</a\\>";
+
+	// replaceAll except for text in between <a> </a> tags
+	public String replaceOutsideATAG(String text, String regex, String replacement) {
+		StringBuffer temp = new StringBuffer();
+		
+		Pattern p = Pattern.compile(HTML_A_TAG);
+		Matcher matcher = p.matcher(text);
+		
+		int lastIndex = 0;
+		
+		while (matcher.find()) {
+			temp.append(text.substring(lastIndex, matcher.start()).replaceAll(regex, replacement));
+			temp.append(matcher.group());
+			
+			lastIndex = matcher.end();
+		}
+		
+		temp.append(text.substring(lastIndex, text.length()).replaceAll(regex, replacement));
+		
+		return temp.toString();
+	}
+	
+	public String removeHTML(String text) {
+		return text.replaceAll(NO_HTML,"");
+	}
+	
+	public String addWEB(String text) {
+		return replaceOutsideATAG(text, WEB_REGEX,"<a href='$1'>$1</a>");
+	}
+
+	public String addMail(String text) {
+		return replaceOutsideATAG(text, EMAIL_REGEX,"<a href='mailto:$1'>$1</a>");
+	}
+	
+	public String addWWW(String text) {
+		return replaceOutsideATAG(text, WWW_REGEX,"<a href='http://$1'>$1</a>");
+	}
+
+	public String makeClickable(String text) {
+		text = removeHTML(text);
+		text = addMail(text);
+		text = addWEB(text);
+		text = addWWW(text);
+
+	    return text;
+	}
+}
---
+++ eclipse-project/src/agitter/ui/helper/HTMLFormatterTest.java
@@ -0,0 +1,90 @@
+package agitter.ui.helper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HTMLFormatterTest {
+
+	HTMLFormatter formatter = new HTMLFormatter();
+	
+	public void equals(String formatted, String test) {
+		assertEquals(formatted, formatter.makeClickable(test));
+	} 
+	
+	@Test
+	public void testSimpleReplacement() {
+		equals("<a href='http://teste.com.br'>http://teste.com.br</a>", 
+				"http://teste.com.br");
+		
+		equals("Hi this <a href='http://teste.com.br'>http://teste.com.br</a> is a test.", 
+			   "Hi this http://teste.com.br is a test.");		
+	}
+
+
+	@Test
+	public void testHTTPS() {
+		equals("Hi this <a href='https://teste.com'>https://teste.com</a>.",
+			   "Hi this https://teste.com."); 
+	}
+	
+	@Test
+	public void testFTP() {
+		equals("Hi this <a href='ftp://teste.com'>ftp://teste.com</a>.",
+			   "Hi this ftp://teste.com."); 
+	}
+	
+	@Test
+	public void testDoubleURL() {
+		equals("Hi this <a href='http://teste.com'>http://teste.com</a> is a test <a href='http://teste.com.br'>http://teste.com.br</a>.",
+			   "Hi this http://teste.com is a test http://teste.com.br."); 
+	}
+	
+	@Test
+	public void testMail() {
+		equals("Hi this <a href='mailto:vitor@vitorpamplona.com'>vitor@vitorpamplona.com</a>.",
+			   "Hi this vitor@vitorpamplona.com."); 
+	}
+	
+	@Test
+	public void testHTMLedMail() {
+		equals("Hi this <a href='mailto:vitor@vitorpamplona.com'>vitor@vitorpamplona.com</a>.",
+			   "Hi this <a href='mailto:vitor@vitorpamplona.com'>vitor@vitorpamplona.com</a>."); 
+	}
+	
+	@Test
+	public void testWWW() {
+		equals("Hi this <a href='http://www.teste.com'>www.teste.com</a>.",
+			   "Hi this www.teste.com."); 
+	}
+	
+	@Test
+	public void testNpWWW() {
+		equals("Hi this <a href='http://teste.com'>teste.com</a>.",
+			   "Hi this teste.com."); 
+	}
+	
+	@Test
+	public void testNothing() {
+		equals("Hi this teste. com. Hey you should do nothing here",
+			   "Hi this teste. com. Hey you should do nothing here"); 
+	}	
+
+	
+	@Test
+	public void testReplaceAllOutsideHTML() {
+		assertEquals("<a href='teste.com.br'>teste</a>", 
+				formatter.replaceOutsideATAG(
+						"<a href='teste.com.br'>teste</a>", "teste", "oi"));
+		
+		assertEquals("ddd oi <a href='teste.com.br'>teste</a>", 
+				formatter.replaceOutsideATAG(
+						"ddd teste <a href='teste.com.br'>teste</a>", "teste", "oi"));
+		
+		assertEquals("ddd oi <a href='teste.com.br'>teste</a> oi", 
+				formatter.replaceOutsideATAG(
+						"ddd teste <a href='teste.com.br'>teste</a> teste", "teste", "oi"));		
+	}
+	
+	
+}
