ccfc698bf762301b265b0747efa0030fa7cfc775
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-01-26 18:21:15 -0200

    Multi-tab spike

--- eclipse-project/src/agitter/main/spike/MultiTabApplication.java
+++ eclipse-project/src/agitter/main/spike/MultiTabApplication.java
@@ -8,11 +8,16 @@ public class MultiTabApplication extends Application {
 
 	@Override
 	public void init() {
-		this.setMainWindow(new MultiTabWindow());
+		MultiTabWindow mainWindow = new MultiTabWindow();
+		this.setMainWindow(mainWindow);
+		System.out.println("Main window name:" + mainWindow.getName());
 	}
 	
 	@Override
 	public Window getWindow(String name) {
+		System.out.println("App URL: " + getURL() + "  " + System.currentTimeMillis());
+		System.out.println("Window name: " + name);
+		
 		// If the window is identified by name, we are good to go
 		Window w = super.getWindow(name);
 
@@ -22,7 +27,7 @@ public class MultiTabApplication extends Application {
 
 			// Use the random name given by the framework to identify this
 			// window in future
-			w.setName(name);
+			w.setName("foo" + name);
 			addWindow(w);
 
 			// Move to the url to remember the name in the future
@@ -32,4 +37,10 @@ public class MultiTabApplication extends Application {
 		return w;
 	}
 
+	public static SystemMessages getSystemMessages() {
+		CustomizedSystemMessages messages = new CustomizedSystemMessages();
+		messages.setOutOfSyncNotificationEnabled(false);
+		messages.setSessionExpiredNotificationEnabled(false);
+		return messages;
+	}
 }
