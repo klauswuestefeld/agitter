a0612b3c83ed4da6dff8a2a6b5382165ebf952fc
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-02-07 15:42:32 -0200

    Simploy cleanup.

--- bubble/src/infra/simploy/SimployTestsRun.java
+++ bubble/src/infra/simploy/SimployTestsRun.java
@@ -77,17 +77,11 @@ public class SimployTestsRun {
 
 		String rootPath = testsFolder().getAbsolutePath();
 
-		System.out.println(rootPath);
-		
 		for (String filePath : classFilePaths) {
 			String className = className(rootPath, filePath);
-		
-			System.out.println(rootPath +"/"+ filePath);
 			
 			Class<?> c = getClass().getClassLoader().loadClass(className);
-			System.out.println(getClass().getClassLoader().toString());
-			System.out.println(c.getClassLoader());
-			System.out.println(c.getResource(".").getPath());
+			System.out.println(c.getResource(c.getSimpleName() + ".class").getPath());
 			
 			if (!Modifier.isAbstract(c.getModifiers()))
 				result.add(c);
