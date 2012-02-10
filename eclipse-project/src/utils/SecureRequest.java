d141c5c58b46061625da65f306c47a08cd6ea5c6
André Faria <andre.java@gmail.com>
2012-02-09 23:45:02 -0200

    Refactoring encoding behavior to spefic class and testing

--- eclipse-project/src/utils/SecureRequest.java
+++ eclipse-project/src/utils/SecureRequest.java
@@ -1,19 +1,12 @@
 package utils;
-import java.security.Key;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
 import sneer.foundation.lang.exceptions.Refusal;
-
-
 
 public abstract class SecureRequest {
 
-	private static final String HMAC_SHA256 = "HmacSHA256";
 	private String params = "";
 	
 	public String asSecureURI() {
@@ -29,7 +22,7 @@ public abstract class SecureRequest {
 	
 	protected String securityCode() {
 		String input = asURI();
-		return computeHmac("QualquerSenha764", input);
+		return new Encoder().computeHmac("QualquerSenha764", input);
 	}
 
 	
@@ -39,40 +32,6 @@ public abstract class SecureRequest {
 		params += params.isEmpty() ? "?" : "&";
 		params += name + "=" + value;
 	}
-	
-	
-	private String computeHmac(String privateKey, String input) {
-			byte[] keyBytes = privateKey.getBytes();
-			Key key = new SecretKeySpec(keyBytes, 0, keyBytes.length, HMAC_SHA256); 
-			Mac mac = getAlgorithm(HMAC_SHA256, key); 
-			return toHex(mac.doFinal(input.getBytes()));
-	}
-
-
-	private Mac getAlgorithm(String algorithm, Key key) {
-		try {
-			Mac mac = Mac.getInstance(algorithm);
-			mac.init(key);
-			return mac;
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-	}
-	
-	
-	private String toHex(byte [] a) {
-		int hn, ln, cx;
-		String hexDigitChars = "0123456789ABCDEF";
-		StringBuffer buf = new StringBuffer(a.length * 2);
-		for(cx = 0; cx < a.length; cx++) {
-			hn = ((int)(a[cx]) & 0x00ff) / 16;
-			ln = ((int)(a[cx]) & 0x000f);
-			buf.append(hexDigitChars.charAt(hn));
-			buf.append(hexDigitChars.charAt(ln));
-		}
-		return buf.toString();
-	}
-
 
 	protected void validate(Map<String, String[]> params) throws Refusal {
 		String[] code = params.get("code");
