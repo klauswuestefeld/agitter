78b53efa1896c89f66bcaf55c6a7abdbffc17400
Vitor Pamplona <vitor@vitorpamplona.com>
2012-02-13 23:14:47 -0200

    BUG DO VAADIN? Perda esporádica de eventos de click na lista de agitos.

--- eclipse-project/src/agitter/ui/view/session/events/EventListViewImpl.java
+++ eclipse-project/src/agitter/ui/view/session/events/EventListViewImpl.java
@@ -45,8 +45,20 @@ final class EventListViewImpl extends CssLayout implements EventListView {
 	private void onEventSelected(LayoutClickEvent layoutClickEvent) {
 		EventListElement eventView = (EventListElement) layoutClickEvent.getChildComponent();
 		if (eventView == null) {
-			System.out.println("Child clicado nao achado. Component Clicked: " + (layoutClickEvent.getClickedComponent() == null) );
-			return;
+			// Vaddin Bug? Searching in a limited parent chain. 
+			Component current = layoutClickEvent.getClickedComponent();
+			for (int i=0; i<5; i++) {
+				if (current == null) return;
+				
+				if (current instanceof EventListElement) {
+					eventView = (EventListElement)current;
+					break;
+				} 
+				
+				current = current.getParent();
+			}
+			System.out.println("Searching: " + current.getClass());
+			if (eventView == null) return;
 		}
 		Object eventObject = eventView.getEventObject();
 		boss.onEventSelected(eventObject);
