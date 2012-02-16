946fdf3c8e51420d91166058b80865dbe77b50ec
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2012-02-15 23:28:53 -0200

    Comments UI. Minor

--- eclipse-project/src/agitter/ui/presenter/InvitePresenter.java
+++ eclipse-project/src/agitter/ui/presenter/InvitePresenter.java
@@ -8,7 +8,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
