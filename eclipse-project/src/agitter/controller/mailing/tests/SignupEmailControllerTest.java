3a4246a7c38f73e433ee13dc2e978d8e70c74c38
Flavio W. Brasil <fwbrasil@gmail.com>
2012-03-09 01:02:21 -0300

    Encode secure urls properly (support signup for emails like "email+1@gmail.com")

--- eclipse-project/src/agitter/controller/mailing/tests/SignupEmailControllerTest.java
+++ eclipse-project/src/agitter/controller/mailing/tests/SignupEmailControllerTest.java
@@ -20,18 +20,33 @@ public class SignupEmailControllerTest extends TestWithMocks {
 	private final SignupEmailController subject = new SignupEmailController(sender, users);
 	
 
-	@Test (timeout = 2000)
+	@Test(timeout = 2000)
 	public void signupViaEmail() throws Exception {
 		checking(new Expectations(){{
 			allowing(users).searchByEmail(email("ana@mail.com"));
 		}});
 		subject.initiateSignup(email("ana@mail.com"), "password123");
-		assertTrue(sender.body().contains("email=ana@mail.com"));
+		assertTrue(sender.body().contains("email=ana%40mail.com"));
 		
 		checking(new Expectations(){{
 			exactly(1).of(users).signup(email("ana@mail.com"), "password123");
 		}});
 		Map<String, String[]> params = map("email", "ana@mail.com", "code", "5CBDF90FAD3E408C93E08F697696CA8A3AC88DC6BB11A7918C4CB31FD5905798");  //Obtained by Regression
+		subject.onRestInvocation(params);
+	}
+	
+	@Test(timeout = 2000)
+	public void signupViaEmailWithPlusCharacter() throws Exception {
+		checking(new Expectations(){{
+			allowing(users).searchByEmail(email("ana+1@mail.com"));
+		}});
+		subject.initiateSignup(email("ana+1@mail.com"), "password123");
+		assertTrue(sender.body().contains("email=ana%2B1%40mail.com"));
+		
+		checking(new Expectations(){{
+			exactly(1).of(users).signup(email("ana+1@mail.com"), "password123");
+		}});
+		Map<String, String[]> params = map("email", "ana+1@mail.com", "code", "C4547596FFCFDA1381212731460137B8EEFF2B45AD650BFDC89328891B6436B3");  //Obtained by Regression
 		subject.onRestInvocation(params);
 	}
 
