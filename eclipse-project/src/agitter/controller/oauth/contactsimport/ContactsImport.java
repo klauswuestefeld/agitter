49ca5b45e17b94431db4f8c1c99c74ea8f363885
Vitor Pamplona <vitor@vitorpamplona.com>
2012-03-24 09:45:49 -0400

    UTest fix: Too many sysouts.

--- eclipse-project/src/agitter/controller/oauth/contactsimport/ContactsImport.java
+++ eclipse-project/src/agitter/controller/oauth/contactsimport/ContactsImport.java
@@ -50,7 +50,6 @@ public class ContactsImport extends Thread {
 	
 	private void importIfNecessary(Contact candidate) {
 		User user = asUser(candidate);
-		System.out.println("Adicionando " + candidate.getDisplayName() + " " + candidate.getEmail() + " " + candidate.getId() + " " + candidate.getProfileUrl() + " " + candidate.getFirstName());
 		
 		if (user == null) return;
 		
